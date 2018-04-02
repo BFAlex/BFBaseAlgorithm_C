7c8a560f9edca951f001cbcd2ae370655bc15610
BFAlex <bfalex@BFAlexdeMac.local>
2018-04-02 09:24:42 +0800

    Initial Commit

---
+++ BFBaseAlgorithm_C/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  BFBaseAlgorithm_C
+//
+//  Created by BFAlex on 2018/4/2.
+//  Copyright © 2018年 BFAlex. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
